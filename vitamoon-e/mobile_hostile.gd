d04467964dc8010c0a0b8c1a84de0cb44ae6453d
Ian Weber <iacweber@gmail.com>
2025-08-07 16:17:58 -0700

    Beginning monster AI

--- vitamoon-e/mobile_hostile.gd
+++ vitamoon-e/mobile_hostile.gd
@@ -1,6 +1,7 @@
+extends CharacterBody3D
 @export var speed: float = 2.0
 var player
-extends StaticBody3D
+
 
 var health = 3
 var static_sprite = load("res://assets_2d/Neutral Pose Evil Sunny-E Guy.png")
@@ -19,3 +20,23 @@ func Hit(damage):
 		$Sprite3D.texture = dead_sprite
 		$Sprite3D.position = $Sprite3D.position - Vector3(0,1,0)
 		$CollisionShape3D.disabled = true
+		
+func _physics_process(delta):
+	if not player or health <= 0:
+		return
+		
+	var to_player = player.global_transform.origin -global_transform.origin
+	var distance = to_player.length()
+	if distance > 1.5:
+		var direction = to_player.normalized()
+		velocity = direction *speed
+		move_and_slide()
+		$Sprite3D.texture = static_sprite
+	else:
+		velocity = Vector3.ZERO
+		move_and_slide()
+		#$Sprite3d.texture= attacking_sprite
+		attack_player()
+		
+func attack_player():
+	print("OOGA BOOGA")
